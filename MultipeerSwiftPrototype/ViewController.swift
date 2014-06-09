cfa8978c53e6d071ca708940554f3cd914f6010d
brett ohland <brettohland@gmail.com>
2014-06-09 14:17:56 -0600

    Initial Commit

---
+++ MultipeerSwiftPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipeerSwiftPrototype
+//
+//  Created by brett ohland on 2014-06-09.
+//  Copyright (c) 2014 ampersandsoftworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
